f3f836739e88ea6373eb2a133d151dcf32b00015
abdessamad <myemail@gmail.com>
2023-03-18 16:36:01 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of int: %lu byte(s)\n", sizeof(b));
 printf("Size of long int: %lu byte(s)\n", sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", sizeof(d));
-printf("Size of float: %lu byte(s)", sizeof(f));
+printf("Size of float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
