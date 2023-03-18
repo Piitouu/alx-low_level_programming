6daee56e0cb125a6f4a4232f4a998a823903da6e
abdessamad <myemail@gmail.com>
2023-03-18 16:24:42 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - min fnc
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	
+printf("Size of a char: %1u byte(s)", sizeof(a));
+printf("Size of int: %1u byte(s)", sizeof(b));
+printf("Size of long int: %1u byte(s)", sizeof(c));
+printf("Size of long long int: %1u byte(s)", sizeof(d));
+printf("Size of float: %1u byte(s)", sizeof(f));
+return (0);
+
+}
